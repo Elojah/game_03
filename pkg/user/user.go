f8285054d14a1041ccd769f79a79b47a4a4ee982
elojah <swingcastor@gmail.com>
2020-12-27 13:50:34 +0100

    wip

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -1,5 +1,22 @@
 package user
 
+import (
+	"context"
+
+	"github.com/elojah/game_03/pkg/ulid"
+)
+
 type App interface {
+	Store
 	CacheSession
+}
+
+type Filter struct {
+	ID       *ulid.ID
+	TwitchID *string
+}
+
+type Store interface {
+	Upsert(context.Context, U) error
+	Fetch(context.Context, Filter) (U, error)
 }
