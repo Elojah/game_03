5142f7ec8aaf03710b1d445cd6dcc3a9e3e13504
hugo <hugo@powder.gg>
2021-05-16 21:29:38 +0200

    misc wip

--- cmd/api/list_pc.go
+++ cmd/api/list_pc.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/elojah/game_03/pkg/entity"
 	"github.com/elojah/game_03/pkg/entity/dto"
 	gerrors "github.com/elojah/game_03/pkg/errors"
+	"github.com/elojah/game_03/pkg/room"
 	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func (h *handler) ListPC(ctx context.Context, req *dto.ListPCReq) (*dto.ListPCResp, error) {
-	logger := log.With().Str("method", "list_room").Logger()
+	logger := log.With().Str("method", "list_pc").Logger()
 
 	if req == nil {
 		return &dto.ListPCResp{}, status.New(codes.Internal, gerrors.ErrNullRequest{}.Error()).Err()
@@ -24,12 +27,31 @@ func (h *handler) ListPC(ctx context.Context, req *dto.ListPCReq) (*dto.ListPCRe
 		return &dto.ListPCResp{}, status.New(codes.Unauthenticated, err.Error()).Err()
 	}
 
+	// #Fetch rooms
+	r, err := h.room.Fetch(ctx,
+		room.Filter{
+			ID: &req.RoomID,
+		},
+	)
+	if err != nil {
+		if errors.As(err, &gerrors.ErrNotFound{}) {
+			logger.Error().Err(err).Msg("room not found")
+
+			return &dto.ListPCResp{}, status.New(codes.NotFound, err.Error()).Err()
+		}
+
+		logger.Error().Err(err).Msg("failed to fetch rooms")
+
+		return &dto.ListPCResp{}, status.New(codes.Internal, err.Error()).Err()
+	}
+
 	// #Fetch pcs
 	pcs, state, err := h.entity.FetchManyPC(ctx,
 		entity.FilterPC{
-			UserID: &ses.UserID,
-			Size:   int(req.Size_),
-			State:  req.State,
+			UserID:  &ses.UserID,
+			WorldID: &r.WorldID,
+			Size:    int(req.Size_),
+			State:   req.State,
 		},
 	)
 	if err != nil {
@@ -39,6 +61,8 @@ func (h *handler) ListPC(ctx context.Context, req *dto.ListPCReq) (*dto.ListPCRe
 	}
 
 	logger.Info().Msg("success")
+
+	fmt.Println(pcs)
 
 	return &dto.ListPCResp{PCs: pcs, State: state}, nil
 }
