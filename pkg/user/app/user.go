ecce0274d81fba99e68e712ad8ed74fc7441225d
hdezier <hugo.dezier@platform.sh>
2022-10-15 16:20:46 +0200

    fix wip

--- pkg/user/app/user.go
+++ pkg/user/app/user.go
@@ -27,6 +27,14 @@ type App struct {
 
 func (a App) CreateJWT(ctx context.Context, u user.U) (string, error) {
 	// #Create JWT
+	id := ulid.NewID()
+
+	// use cookie rotation encoding to generate a rotating secret for JWT
+	secret, err := a.Cookie.Encode(ctx, "jwt_secret", id.String())
+	if err != nil {
+		return "", err
+	}
+
 	now := time.Now()
 	claims := &user.Claims{
 		RegisteredClaims: jwt.RegisteredClaims{
@@ -36,19 +44,13 @@ func (a App) CreateJWT(ctx context.Context, u user.U) (string, error) {
 			ExpiresAt: jwt.NewNumericDate(now.Add(time.Hour)),
 			NotBefore: jwt.NewNumericDate(now),
 			IssuedAt:  jwt.NewNumericDate(now),
-			ID:        ulid.NewID().String(),
+			ID:        secret,
 		},
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	// use cookie rotation encoding to generate a rotating secret for JWT
-	secret, err := a.Cookie.Encode(ctx, "jwt_secret", claims.RegisteredClaims.ID)
-	if err != nil {
-		return "", err
-	}
-
-	ss, err := token.SignedString([]byte(secret))
+	ss, err := token.SignedString([]byte(id.String()))
 	if err != nil {
 		return "", err
 	}
@@ -62,7 +64,12 @@ func (a App) CreateJWT(ctx context.Context, u user.U) (string, error) {
 }
 
 func (a App) ReadJWT(ctx context.Context, token string) (user.U, error) {
-	t, err := jwt.ParseWithClaims(token, &user.Claims{}, func(t *jwt.Token) (interface{}, error) {
+	ck, err := a.Cookie.Decode(ctx, "token", token)
+	if err != nil {
+		return user.U{}, err
+	}
+
+	t, err := jwt.ParseWithClaims(ck, &user.Claims{}, func(t *jwt.Token) (interface{}, error) {
 		claims, ok := t.Claims.(*user.Claims)
 		if !ok {
 			return user.U{}, errors.ErrInvalidClaims{}
@@ -118,12 +125,7 @@ func (a App) Auth(ctx context.Context) (user.U, error) {
 		return user.U{}, errors.ErrMissingAuth{}
 	}
 
-	s, err := a.Cookie.Decode(ctx, "token", token)
-	if err != nil {
-		return user.U{}, err
-	}
-
-	return a.ReadJWT(ctx, s)
+	return a.ReadJWT(ctx, token)
 }
 
 func (a App) AuthSession(ctx context.Context) (user.Session, error) {
